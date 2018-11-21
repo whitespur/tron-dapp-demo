8d4dad38b4af663a21b00bd3ced5cc41f3ff9450
CNaiko <460078027@qq.com>
2018-11-21 11:57:02 +0800

    tronweb

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    async componentDidMount() {
+
+        let tronWeb;
+        // 1. check tronlink install
+        if (window.tronWeb) {
+            tronWeb = window.tronWeb;
+        } else {
+            alert('plz unlock or install your tronlink');
+            console.error('tronLink not found');
+            return
+        }
+
+        // 2. check node connection
+        const nodes = await tronWeb.isConnected();
+        console.log(nodes)
+        const connected = !Object.entries(nodes).map(([name, connected]) => {
+            if (!connected) {
+                console.error(`Error: ${name} is not connected`);
+            }
+            return connected;
+        }).includes(false);
+        if (!connected){
+            alert(`Error: TRON node is not connected`);
+            return;
+        }
+
+        // 3. get balance
+        tronWeb.trx.getBalance(tronWeb.defaulAddress).then(balance => {
+            console.log({balance});
+        }).catch(err => console.error(err));
+
+
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    > Learn React </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+console.log(window.tronWeb)
 ReactDOM.render(<App />, document.getElementById('root'));
-
+console.log(window.tronWeb)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA

44871c08c3e3ff17a678a40569061340ee077cbe
CNaiko <460078027@qq.com>
2018-11-21 18:08:44 +0800

    add wait for global var  set logic

--- src/App.js
+++ src/App.js
@@ -3,34 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 
+
 class App extends Component {
 
     async componentDidMount() {
-
-        let tronWeb;
-        // 1. check tronlink install
-        if (window.tronWeb) {
-            tronWeb = window.tronWeb;
-        } else {
-            alert('plz unlock or install your tronlink');
-            console.error('tronLink not found');
-            return
-        }
-
-        // 2. check node connection
-        const nodes = await tronWeb.isConnected();
-        console.log(nodes)
-        const connected = !Object.entries(nodes).map(([name, connected]) => {
-            if (!connected) {
-                console.error(`Error: ${name} is not connected`);
-            }
-            return connected;
-        }).includes(false);
-        if (!connected){
-            alert(`Error: TRON node is not connected`);
-            return;
-        }
-
+        let tronWeb = window.tronWeb;
         // 3. get balance
         tronWeb.trx.getBalance(tronWeb.defaulAddress).then(balance => {
             console.log({balance});
--- src/index.js
+++ src/index.js
@@ -2,11 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-console.log(window.tronWeb)
-ReactDOM.render(<App />, document.getElementById('root'));
-console.log(window.tronWeb)
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+var waitForGlobal = async () =>{
+    // 1. check variable
+    if (window.tronWeb) {
+        let tronWeb = window.tronWeb;
+        // 2. check node connection
+        const nodes = await tronWeb.isConnected();
+        const connected = !Object.entries(nodes).map(([name, connected]) => {
+            if (!connected) {
+                console.error(`Error: ${name} is not connected`);
+            }
+            return connected;
+        }).includes(false);
+        if (connected){
+            ReactDOM.render(<App />, document.getElementById('root'));
+        } else {
+            alert(`Error: TRON node is not connected`);
+        }
+
+    } else {
+        console.error('wait for tronLink');
+        setTimeout(async () => {
+            await waitForGlobal();
+        }, 100);
+    }
+};
+
+waitForGlobal();
+

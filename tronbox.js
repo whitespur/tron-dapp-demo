9a8cc4431591fc98122835241ccc320cacf5f0a4
max <4012505+maxisacoder@users.noreply.github.com>
2018-12-03 12:01:24 +0800

    Update tronbox.js

--- tronbox.js
+++ tronbox.js
@@ -1,7 +1,7 @@
 module.exports = {
   networks: {
     shasta: {
-      privateKey: '7c3f3b3826eede41c420fe02a41f1fc0ef4465fb1a6af4591f28382fdc81deae',
+      privateKey: '你的私钥',
       consume_user_resource_percent: 30,
       fee_limit: 100000000,
       fullNode: "https://api.shasta.trongrid.io",
